e00dddefe64015f81e17a1bfffc3d695ae9a22db
不死不朽 <1933292552@qq.com>
2020-07-02 17:06:30 +0800

    feat: 模拟业务逻辑

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { execSync } = require('child_process')
 const express = require('express')
 
 const app = express()
@@ -5,8 +6,32 @@ const app = express()
 const PORT = 2333
 const HOST = '0.0.0.0'
 
-app.post('/', (req, res) => {
-  console.log(req)
+let stack = []
+
+app.get('/', (req, res) => {
+  //校验身份
+  let role = false
+  console.log('校验身份');
+  role = true
+  if (!role) {
+    return
+  }
+
+  let info  = req
+
+  if (stack.length === 0) {
+    // 任务入栈
+    stack.push(info)
+
+    console.log('执行构建代码');
+    execSync('ls')
+    
+    // 任务出栈
+    stack = []
+  } else {
+    console.log('有任务正在执行');
+  }
+
 
   res.status(200)
   res.send('It is ok')
